a58fc36e6c427bc7626a66ddfe1a8c6032b378bf
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-03-29 15:48:18 -0400

    Add files via upload

--- letschat_room.js
+++ letschat_room.js
@@ -21,5 +21,36 @@ function addRoom()
 
 function logout()
 {
-    window.location = "index.html"
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location.replace("index.html");
 }
+
+function getData()
+{
+      firebase.database().ref("/").on('value', function(snapshot)  {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+      console.log("room_name - " + room_name)
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id') >#"+ Room_names +"</div><hr>"
+      document.getElementById("output").innerHTML += row;
+      user_name = localStorage.getItem("user_name");
+      document.getElementById("user_name").innerHTML = "welcome " + user_name + "!";
+
+      function addRoom()
+      {
+            room_name = document.getElementById("room_name").value;
+            firebase.database().ref("/").child(room_name).update({
+            perpose : "adding room name"
+            });
+            localStorage.setItem("room_name", room_name);
+            window.location = "kwitter_page.html";
+
+            function redirectToRoomName(name)
+            {
+                  console.log(name);
+                  localStorage.setItem("room_name", room_name);
+                  window.location = "kwitter_page.html";
+            }
+}
+});});}
+getData(); 
